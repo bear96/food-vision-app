baf7e2432c3eb73eb76729b658f686dfa08a4b7a
Aritra Majumdar <aritram21@vt.edu>
2023-03-13 22:10:59 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import torch
+import numpy as np
 import torchvision.transforms as transforms
 from flask import Flask, request, app, jsonify, url_for, render_template, redirect, flash, session, escape
+from PIL import Image
 
 
 app = Flask(__name__)
-model = torch.jit.load('resnet_food.pt',map_location=torch.device("cpu"))
-model.eval()
-
+labels = {}
+i=0
+with open("labels.txt") as f:
+    for line in f:
+        labels[i]= line.strip()
+        i+=1
 
 @app.route('/')
 def home():
@@ -14,15 +19,20 @@ def home():
 
 @app.route('/predict_api', methods = ['POST'])
 def predict_api():
-    data = request.json['data']
-    print(data)
+    model = torch.jit.load('resnet_food.pt', map_location=torch.device('cpu'))
+    model.eval()
+    data = request.files['image']
+    img = Image.open(data.stream)
+    print("Img height and img width: ", img.height,img.width)
     transformations = transforms.Compose([transforms.Resize((224,224)),
                                           transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    input_data = transformations(data)
+    input_data = transformations(img)
+    input_data = input_data.unsqueeze(0)
     output = model(input_data)
-    _, pred = torch.max(output)
-    print(pred)
-    return(jsonify(pred))
+    pred = torch.argmax(output)
+    conf = torch.max(output)
+    result = labels[pred.item()]
+    return(jsonify(result,conf.item()*100))
 
 
 
